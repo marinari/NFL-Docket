f6593f1e745b016c30ce69f188b2419531c8dfea
Arianna Marin <marin.arianna@gmail.com>
2016-07-24 20:36:59 -0700

    yeoman and angularjs packages installed

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name nflDocketApp
+ * @description
+ * # nflDocketApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('nflDocketApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nflDocketApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the nflDocketApp
+ */
+angular.module('nflDocketApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
