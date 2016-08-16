11f60240f56bbc1eee0f35417549dfdce0159df5
Arianna Marin <marin.arianna@gmail.com>
2016-08-16 00:24:55 -0700

    fixed error, full schedule now loads in about view, angular transitions work

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -7,14 +7,14 @@
  * # AboutCtrl
  * Controller of the nflDocketApp
  */
- 
-  angular.module('nflDocketApp')
-    .controller('MainCtrl', function ($scope, current) {
-      $scope.current = current.query();
+
+angular.module('nflDocketApp')
+  .controller('AboutCtrl', function ($scope, current) {
+    $scope.current = current.query();
       $scope.selectteam = function (teamname){
-        console.log ("selected team: " + teamname);
+        /*console.log ("selected team: " + teamname); this line is giving error warnings */
         $scope.selectedteam = teamname;
         $scope.current = current.query();
-      };
+    };
 
-    });
+});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,17 +8,11 @@
  * Controller of the nflDocketApp
  */
 
-angular.module('nflDocketApp')
-  .controller('MainCtrl', function ($scope, current) {
-    $scope.current = current.query();
-    $scope.selectteam = function (teamname){
-      console.log ("selected team: " + teamname);
-      $scope.selectedteam = teamname;
-      $scope.current = current.query();
-    };
-
-  });
-
+ angular.module('nflDocketApp')
+   .controller('MainCtrl', function ($scope, current) {
+     $scope.current = current.query();
+   });
+   
 /*
   function formatJSONDate(jsonDate) {
     var newDate = dateFormat(jsonDate, "mm/dd/yyyy");
